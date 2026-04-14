83a244e1bd437d042140c3b11d6ff98b96d419c7
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2026-04-14 18:16:42 +0000

    minor additions part 2

--- module.py
+++ module.py
@@ -8,7 +8,11 @@
 class PointOfSale:
   def __init__(self):
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
-    Fruits = {"oranges": 50, "apples": 50, "bananas": 50, "avocado": 50}
+    Fruits = {"oranges": int(50), "apples": int(50), "bananas": int(50), "avocado": int(50)}
+    Vegetables = {"yuca": int(50), "carrots": int(50), "pumpkin": int(50)}
+    Drinks = {"Coca-Cola": int(50), "Dr. Pepper": int(50), "Sprite": int(50), "Orange Juice": int(50), "Apple Juice": int(50)}
+    FastFood = {"Cheeseburger": int(50), "Chicken Burger": int(50), "Chili Dog": int(50), "Nachos": int(50), "Pizza": int(50)}
+    Menu = [Fruits, Vegetables, Drinks, FastFood]
     print("\nInitializing POS system...")
     
     
