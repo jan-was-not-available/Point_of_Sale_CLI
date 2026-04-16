ba2f3f20a5b9554c896b3164f3a9f8768276d856
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2026-04-16 18:17:20 +0000

    additions, good pacing

--- module.py
+++ module.py
@@ -8,13 +8,33 @@
 class PointOfSale:
   def __init__(self):
     self.checkout_total = 0 # This is an example varialbe, remove it or change it as you please.
-    Fruits = {"oranges": int(50), "apples": int(50), "bananas": int(50), "avocado": int(50)}
-    Vegetables = {"yuca": int(50), "carrots": int(50), "pumpkin": int(50)}
-    Drinks = {"Coca-Cola": int(50), "Dr. Pepper": int(50), "Sprite": int(50), "Orange Juice": int(50), "Apple Juice": int(50)}
-    FastFood = {"Cheeseburger": int(50), "Chicken Burger": int(50), "Chili Dog": int(50), "Nachos": int(50), "Pizza": int(50)}
-    Menu = [Fruits, Vegetables, Drinks, FastFood]
+    self.Cart = []
+    self.Fruits = ["oranges", "apples", "banana", "avocado"]
+    self.Vegetables = ["yuca", "carrots", "pumpkins", "spinach"]
+    self.Drinks = ["coke", "dr.pepper", "sprite", "fanta", "apple juice", "orange juice"]
+    self.FastFood = ["cheeseburger", "chicken burger", "chili dog", "nachos", "pizza"]
     print("\nInitializing POS system...")
     
     
   def start(self): # This is the function that should be used to start the application.
-    print("\nThe app is not complete.")
+    print("=======================================")
+    print("\nWelcome to Alejandro's Supermarket...")
+    print("\n=======================================")
+    while len(self.Cart) <= 0:
+      print("What would you like to do today?")
+      print("\nOptions:")
+      print("1 - Show Cart")
+      print("2 - Add Item to Cart")
+      print("3 - Show Checkout Total")
+      print("4 - Finalize Payment")
+      print("5 - Exit")
+      choice = input("\nEnter Selection: ")
+      print("Message: ")
+      if choice == "1":
+        print(self.Cart)
+
+      elif choice == "2":
+        print("What would you like to add?")
+        print("Fruits: ", self.Fruits, "Vegetables: ", self.Vegetables, "Drinks: ", self.Drinks, "Fast Food: ", self.FastFood)
+
+      break
