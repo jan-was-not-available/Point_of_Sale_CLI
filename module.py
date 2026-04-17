367ab509838623cdddb87256e20b4168d2aed01a
jan-was-not-available <alejandro.oliveroasencio@stu.vertexacademies.org>
2026-04-17 18:16:35 +0000

    Full Finish 4/17/26

--- module.py
+++ module.py
@@ -23,6 +23,8 @@ class PointOfSale:
       if not self.Cart:
           print("No items purchased")
           return
+      
+      total = 0 
 
       prices = {
         "oranges": 1.50,
@@ -32,16 +34,30 @@ class PointOfSale:
         "yuca": 2.20,
         "carrots": 0.50,
         "pumpkins": 3.50,
-        "spinach": 0.75
-      }  
-
+        "spinach": 0.75,
+        "coke": 2.00,
+        "dr.pepper": 2.15,
+        "sprite": 2.20,
+        "fanta": 2.05,
+        "apple juice": 3.50,
+        "orange juice": 3.75,
+        "cheeseburger": 4.00,
+        "chicken burger": 3.50,
+        "chili dog": 2.50,
+        "nachos": 7.50,
+        "pizza": 12.00
+      }
+      
       for item in self.Cart:
           price = prices.get(item, 0)
-          print(f"{item.title():15} $ {price: 2f}")
-      
-      print("---------------------------------")
-      print(f"TOTAL:          $ {self.checkout_total:.2f}")
-      print("---------------------------------\n")
+          total += price
+          print(f"{item:15} $ {price:.2f}")
+
+      print("-----------------------------")
+      print(f"TOTAL:           $ {total:.2f}")
+        
+
+    
 
   def start(self): # This is the function that should be used to start the application.
     print("=======================================")
@@ -56,6 +72,7 @@ class PointOfSale:
       print("3 - Show Checkout Total")
       print("4 - Finalize Payment")
       print("5 - Exit")
+      print("6 - Remove Item(s)")
 
       choice = input("\nEnter Selection: ")
 
@@ -80,6 +97,17 @@ class PointOfSale:
           "carrots": 0.50,
           "pumpkins": 3.50,
           "spinach": 0.75,
+          "coke": 2.00,
+          "dr.pepper": 2.15,
+          "sprite": 2.20,
+          "fanta": 2.05,
+          "apple juice": 3.50,
+          "orange juice": 3.75,
+          "cheeseburger": 4.00,
+          "chicken burger": 3.50,
+          "chili dog": 2.50,
+          "nachos": 7.50,
+          "pizza": 12.00
         }
 
         if itemchoice in prices:
@@ -90,7 +118,7 @@ class PointOfSale:
           print("Item not recognized :( ")
 
       elif choice == "3":
-        print("\Checkout Total: $", round(self.checkout_total, 2))
+        print("\nCheckout Total: $", round(self.checkout_total, 2))
 
       elif choice == "4":
         self.print_receipt()
@@ -99,8 +127,25 @@ class PointOfSale:
         self.checkout_total = 0.0
 
       elif choice == "5":
-        print("Goodbye!")
+        print("Goodbye! Come again soon!")
         break
+      elif choice == "6":
+        if not self.Cart:
+            print("\nCart is empty. Nothing to remove.")
+        else:
+            print("\nYour cart:")
+            for item in self.Cart:
+                print("-", item)
+            
+            item_to_remove = input("\nType item to remove: ").lower()
 
+            if item_to_remove in self.Cart:
+                self.Cart.remove(item_to_remove)
+
+                if item_to_remove in prices:
+                    self.checkout_total -= prices[item_to_remove]
+                print(f"{item_to_remove} removed from cart.")
+            else:
+                print("Item not found in cart.")
       else:
         print("Invalid option. Try again")
